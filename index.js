ccc2d32c4d6a8d7bdb605d60987929f784a6edcc
Luca Steeb <contact@luca-steeb.com>
2018-03-03 22:05:33 +0100

    support @-webkit-keyframes

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ Less2Sass.prototype.convertInterpolatedVariables = function() {
 
 Less2Sass.prototype.convertVariables = function() {
   // Matches any @ that doesn't have 'media ' or 'import ' after it.
-  var atRegex = /@(?!(media|import|mixin|font-face|keyframes)(\s|\())/g;
+  var atRegex = /@(?!(media|import|mixin|font-face|keyframes|-webkit-keyframes)(\s|\())/g;
 
   this.file = this.file.replace(atRegex, '$');
 
